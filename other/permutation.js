413f435cba5b4eb6ee17dc87064033caec9bef57
color1004 <1083237997@qq.com>
2018-04-22 13:00:55 +0800

    Update permutation.js

--- other/permutation.js
+++ other/permutation.js
@@ -10,16 +10,16 @@ function permutations(arr) {
 	fn(0);
 	function fn(n) {  //为第n个位置选择元素  
 		for(var i=n;i<arr.length;i++) {  
-            swap(arr,i,n);  
-            if(n+1<arr.length-1) { //判断数组中剩余的待全排列的元素是否大于1个  
-				fn(n+1); //从第n+1个下标进行全排列  
-			} else {
-				ans.push(arr); //获得一组结果  
-				ansobj[arr.join("")]=null;  //去重结果
-				ansset.add(arr.join(""));
-			}
-            swap(arr,i,n);  
-        }  
+		    swap(arr,i,n);  
+		    if(n+1<arr.length-1) { //判断数组中剩余的待全排列的元素是否大于1个  
+					fn(n+1); //从第n+1个下标进行全排列  
+				} else {
+					ans.push(arr); //获得一组结果  
+					ansobj[arr.join("")]=null;  //去重结果
+					ansset.add(arr.join(""));
+				}
+		    swap(arr,i,n);  
+		}  
 	}  
 	console.log(ans);
 	console.log(Object.keys(ansobj));
